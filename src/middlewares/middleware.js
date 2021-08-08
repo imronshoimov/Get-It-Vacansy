69163fb31b1b71d110fbfbaae237c5fc662be282
imronshoimov <imronshoimov125@gmail.com>
2021-08-09 00:24:44 +0500

    change middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-    if((req.url == '/login') && req.cookies.token) return res.redirect('/')
-    if(req.url == '/home/recruite' && !req.cookies.token) return res.redirect('/login')
-    if(req.url == '/home/job' && !req.cookies.token) return res.redirect('/login')
+    if((req.url == '/login') && (req.cookies.token && req.cookies.userId)) return res.redirect('/')
+    if(req.url == '/home/recruite' && !(req.cookies.token && req.cookies.userId)) return res.redirect('/login')
+    if(req.url == '/home/job' && !(req.cookies.token && req.cookies.userId)) return res.redirect('/login')
 
     next()
 }   
