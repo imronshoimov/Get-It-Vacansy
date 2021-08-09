e400d5f3d77ed289e6a2cd6e61403c088b11d95b
imronshoimov <imronshoimov125@gmail.com>
2021-08-09 14:06:56 +0500

    add middleware to staffs

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -2,6 +2,8 @@ module.exports = (req, res, next) => {
     if((req.url == '/login') && (req.cookies.token && req.cookies.userId)) return res.redirect('/')
     if(req.url == '/home/recruite' && !(req.cookies.token && req.cookies.userId)) return res.redirect('/login')
     if(req.url == '/home/job' && !(req.cookies.token && req.cookies.userId)) return res.redirect('/login')
+    if(req.url == '/list/recruite' && !(req.cookies.token && req.cookies.userId)) return res.redirect('/login')
+    if(req.url == '/list/job' && !(req.cookies.token && req.cookies.userId)) return res.redirect('/login')
 
     next()
 }   
