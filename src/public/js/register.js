d9fad097ba90563df091356cd67670d207904235
imronshoimov <imronshoimov125@gmail.com>
2021-09-09 22:59:59 +0500

    add switch-button in register

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,7 +1,17 @@
 const imageUpload = document.querySelector('.image-upload')
 const fileInput = document.querySelector('#file-input')
 const fileAvatar = document.querySelector('.file-avatar')
+const labelRecruiter = document.querySelector('.label-recruiter')
+const labelEmployee  = document.querySelector('.label-employee')
 
-fileInput.addEventListener('input', () => {
-    fileAvatar.src = fileInput.files[0].name
-})  
+labelRecruiter.addEventListener('click', () => {
+    switchRecriuter.checked = "true";
+})
+
+labelEmployee.addEventListener('click', () => {
+    switchEmployee.checked = "true";
+})
+
+// fileInput.addEventListener('input', () => {
+//     fileAvatar.src = fileInput.files[0].name
+// })  
